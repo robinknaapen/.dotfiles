09ce15349e21826eafb9fe7da788ca71f0c16b91
robinknaapen <robin@ultraware.nl>
2025-02-24 17:18:44 +0100

    Update .config/hypr/hypr.d/autostart.conf Update .config/nvim/lua/plugins/copilot.lua

--- private_dot_config/nvim/lua/plugins/copilot.lua
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,15 @@
 return {
 	"zbirenbaum/copilot.lua",
 	opts = {
-		markdown = true,
+		filetypes = {
+			markdown = true,
+			sh = function()
+				if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+					return false
+				end
+				return true
+			end,
+		},
 		suggestion = { enabled = false },
 		panel = { enabled = false },
 	},
